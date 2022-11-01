ff19cf911c6852224c9bf39c1dd34b6a8def5bfa
laing raven <laingraven.java@gmail.com>
2022-11-02 00:24:36 +0700

    first commit

---
+++ src/toggle/ToggleAdapter.java
@@ -0,0 +1,12 @@
+package toggle;
+
+public abstract class ToggleAdapter implements ToggleListener {
+
+    @Override
+    public void onSelected(boolean selected) {
+    }
+
+    @Override
+    public void onAnimated(float animated) {
+    }
+}
---
+++ src/toggle/ToggleListener.java
@@ -0,0 +1,8 @@
+package toggle;
+
+public interface ToggleListener {
+
+    public void onSelected(boolean selected);
+
+    public void onAnimated(float animated);
+}
